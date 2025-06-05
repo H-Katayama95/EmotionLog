04873428b034525ad393e9d1157b248dc339836c
honoka <h.katayama@vision-sys.co.jp>
2025-06-05 13:30:12 +0900

    目標進捗テーブルのモデルを追加

---
+++ EmotionLog/Models/RoutineCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionLog.Models
+{
+    public class RoutineCheck
+    {
+        public int RoutineCheckId { get; set; }
+        public int GoalId { get; set; }
+        public DateTime RecordDate { get; set; }
+        public bool IsChecked { get; set; }
+        public int Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ EmotionLog/Repositories/RoutineCheckRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using EmotionLog.Models;
+using Npgsql;
+
+namespace EmotionLog.Repositories
+{
+    public class RoutineCheckRepository : BaseRepository
+    {
+        public List<RoutineCheck> GetRoutineCheck()
+        {
+            var list = new List<RoutineCheck>();
+
+            using var connection = GetConnection();
+            using var command = connection.CreateCommand();
+            connection.Open();
+
+            command.CommandText = "SELECT * FROM routine_check;";
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                list.Add(new RoutineCheck
+                {
+                    RoutineCheckId = reader.GetInt32(0),
+                    GoalId = reader.GetInt32(1),
+                    RecordDate = reader.GetDateTime(2),
+                    IsChecked = reader.GetBoolean(3),
+                    Total = reader.GetInt32(4),
+                    CreatedAt = reader.GetDateTime(5),
+                    UpdatedAt = reader.GetDateTime(6)
+                });
+            }
+            return list;
+        }
+    }
+}
+
